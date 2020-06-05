02f0ce2cedfee61d1c2085a2b58d3fa05cee1cc5
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-05 00:39:13 -0300

    Aula 01 - Conhecendo Arrays

---
+++ src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -0,0 +1,28 @@
+package br.com.bytebank.banco.test;
+
+public class TesteArrayDePrimitivos {
+
+    public static void main(String[] args) {
+
+        int[]idades = new int[5];   //inicializa o array com os valores padrões
+
+//        idades[0] = 29;
+//        idades[1] = 39;
+//        idades[2] = 49;
+//        idades[3] = 59;
+//        idades[4] = 69;//
+//        int idade1 = idades[4];//
+//        System.out.println(idade1);//
+//        System.out.println(idades.length);
+
+        for (int i = 0; i < idades.length; i++) {
+            idades[i] = i * i;
+        }
+
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println(idades[i]);
+        }
+
+
+    }
+}
