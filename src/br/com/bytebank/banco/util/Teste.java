f9abff42b245d6fcce36b57b59867abb45bd8b83
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-07 22:31:29 -0300

    Aula 03 Arrays e Generics

---
+++ src/br/com/bytebank/banco/util/Teste.java
@@ -0,0 +1,49 @@
+package br.com.bytebank.banco.util;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+import java.util.ArrayList;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        ArrayList<Conta> lista = new ArrayList<Conta>();
+
+        Conta cc = new ContaCorrente(22,11);
+        lista.add(cc);
+
+        Conta cc2 = new ContaCorrente(22,22);
+        lista.add(cc2);
+
+        System.out.println("Tamanho: " + lista.size());
+
+        Conta ref = lista.get(0);
+
+        System.out.println(ref.getNumero());
+
+        lista.remove(0);
+        System.out.println("Tamanho: " + lista.size());
+
+        Conta cc3 = new ContaCorrente(33,311);
+        lista.add(cc3);
+
+        Conta cc4 = new ContaCorrente(33,322);
+        lista.add(cc4);
+
+        System.out.println("Metodo FOR tradicional");
+        for (int i = 0; i < lista.size(); i++) {
+            Object oRef = lista.get(i);
+            System.out.println(oRef);
+        }
+        System.out.println("---------------------------------------------------");
+
+        System.out.println("Metodo FOREACH - tem a mesma funçaõ do tradicional, porém, mais utilizado");
+        for (Conta conta:lista) {
+            System.out.println(conta);
+
+        }
+    }
+}
