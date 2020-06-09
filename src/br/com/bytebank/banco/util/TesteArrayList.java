dd3a01d1b0628f97236fa19cbd6a9f9874e9784d
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-08 23:30:50 -0300

    Aula 05 - Vector e interface Collection

--- src/br/com/bytebank/banco/util/TesteArrayList.java
+++ src/br/com/bytebank/banco/util/TesteArrayList.java
@@ -6,13 +6,17 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 public class TesteArrayList {
 
     public static void main(String[] args) {
 
-        List<Conta> lista = new ArrayList<Conta>();
-        ArrayList<Conta> nomes = new ArrayList<Conta>();
+//        List<Conta> lista = new ArrayList<Conta>();
+//        LinkedList<Conta> lista = new LinkedList<Conta>();    //Totalmente encadeada
+//        ArrayList<Conta> nomes = new ArrayList<Conta>();
+
+        List<Conta> lista = new Vector<Conta>();    //Thread Safe
 
         Conta cc = new ContaCorrente(22,11);
         lista.add(cc);
