acf081420d2e3de761783d69eba0fbd9012b4af1
Cassio Murilo <cmurilo.san@gmail.com>
2020-06-11 19:13:04 -0300

    Aula 08 - Classes anônimas e Lambdas | Finalmente Lambdas

--- src/br/com/bytebank/banco/util/Teste2.java
+++ src/br/com/bytebank/banco/util/Teste2.java
@@ -8,6 +8,7 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Teste2 {
 
@@ -43,29 +44,16 @@ public class Teste2 {
         lista.add(cc3);
         lista.add(cc4);
 
-        lista.sort(new Comparator<Conta>() {
+        lista.sort((c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
 
-        @Override
-        public int compare(Conta c1, Conta c2) {
-
-            return Integer.compare(c1.getNumero(), c2.getNumero());
-
-            }
-        });
-
-        Comparator<Conta> comp = new Comparator<Conta>() {
-
-            @Override
-            public int compare(Conta c1, Conta c2) {
-
-                String nomeC1 = c1.getTitular().getNome();
-                String nomeC2 = c2.getTitular().getNome();
-                return nomeC1.compareTo(nomeC2);
-            }
+        Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+            String nomeC1 = c1.getTitular().getNome();
+            String nomeC2 = c2.getTitular().getNome();
+            return nomeC1.compareTo(nomeC2);
         };
 
-        for (Conta conta: lista) {
-            System.out.println(conta + ", " + conta.getTitular().getNome());
-        }
+        lista.sort(comp);
+
+        lista.forEach((conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
     }
 }
